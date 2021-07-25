a64ca1f1cca89f02463048a32f3b44dfd6d3d163
Jennilyn Natad <jennilynnatad98@gmail.com>
2021-07-25 08:47:06 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+
+
+  
+public class Program
+{ 
+    public static void Main()
+    {
+    
+    
+        Console.WriteLine("Input no. of days:" );
+        int number_of_days = Convert.ToInt32(Console.ReadLine());
+
+        int year = number_of_days / 365 ;
+        Console.WriteLine(number_of_days / 365 + " Year(s)");
+
+        number_of_days = number_of_days-(365*year);
+
+        int month = number_of_days / 30;
+        Console.WriteLine(number_of_days / 30 + " Month(s)");
+
+        Console.WriteLine(number_of_days - (month * 30) + " Day(s)");
+
+
+        
+    }
+
+}
